bb3cfbf85ab9a99d76dd01d8f2fd9f76eee82793
Mahmoud Shahin <mahmoudshahin1111@gmail.com>
2023-02-05 04:41:29 +0200

    created base trackable object

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Trackable Object
+
+Track object deeply 🔍 so you can check if it changed or any of it's nested properties had changed.
+
--- src/index.js
+++ src/index.js
@@ -1,52 +1,131 @@
-const changes = new Map();
-function TrackableObjectOf(obj) {
-  obj["_changes"] = new Map();
-  convertPropertyToShadowObj(obj);
-  return obj;
-}
-
-function convertPropertyToShadowObj(obj, propertyKey = null) {
-  if (propertyKey === null && typeof obj === "object") {
-    Object.keys(obj).forEach((key) => {
-      obj[key] = convertPropertyToShadowObj(obj, key);
-    });
-  } else if (propertyKey && typeof obj[propertyKey] === "object") {
-    Object.keys(obj[propertyKey]).forEach((key) => {
-      obj[key] = convertPropertyToShadowObj(obj[propertyKey], key);
-    });
-  } else {
-    ShadowObjectOf(obj, propertyKey);
+function TrackableObject(obj, config) {
+  let basePropertyPath = config?.propertyPath;
+  let changes = config?.changes;
+  if (!changes) {
+    changes = new Map();
   }
-}
 
-function ShadowObjectOf(obj, propertyKey) {
-  const key = Math.round(Math.random() * 10000).toString();
-  Object.defineProperty(obj, "_trackable_obj", { value: {} });
-  Object.defineProperty(obj["_trackable_obj"], "key", { value: key });
-  Object.defineProperty(obj["_trackable_obj"], "value", {
-    value: obj[propertyKey],
-  });
-  console.log(obj,propertyKey,obj[propertyKey]);
-  Object.defineProperty(obj, propertyKey, {
-    get: () => {
-      return obj["_trackable_obj"].value;
-    },
-    set: (v) => {
-      obj["_trackable_obj"].value = v;
-      changes.set(propertyKey, v);
-    },
-  });
+  const _objShadow = JSON.parse(JSON.stringify(obj));
+  function getObjPropertyByPath(_obj, propertyPath) {
+    let propertyValue = _obj;
+    String(propertyPath)
+      .split(".")
+      .forEach((property) => {
+        if (!propertyValue[property]) {
+          propertyValue = null;
+          return false;
+        }
+        propertyValue = propertyValue[property];
+      });
+    return propertyValue;
+  }
+  function overrideSetterAndGetter(_obj, propertyPath) {
+    let propertyValue = _obj;
+    if (propertyPath) {
+      propertyValue = getObjPropertyByPath(_obj, propertyPath);
+    }
+
+    if (typeof propertyValue === "object") {
+      Object.keys(propertyValue).forEach((_key) => {
+        overrideSetterAndGetter(
+          _obj,
+          propertyPath ? `${propertyPath}.${_key}` : _key
+        );
+      });
+    } else {
+      let value = JSON.parse(JSON.stringify(propertyValue));
+      const pathProperties = String(propertyPath).split(".");
+
+      if (pathProperties.length > 1) {
+        const lastProperty = pathProperties[pathProperties.length - 1];
+
+        propertyValue = getObjPropertyByPath(
+          _obj,
+          pathProperties.slice(0, pathProperties.length - 1).join(".")
+        );
+
+        Object.defineProperty(propertyValue, lastProperty, {
+          get() {
+            return value;
+          },
+          set(v) {
+          
+            const oldValue = getObjPropertyByPath(_objShadow, propertyPath);
+            const fullPropertyPath = basePropertyPath? `${basePropertyPath}.${propertyPath}`:propertyPath;
+            if (oldValue === v) {
+              changes.delete(fullPropertyPath);
+            } else {
+            
+              changes.set(fullPropertyPath, JSON.parse(JSON.stringify(v)));
+            }
+            const clonedValue = JSON.parse(JSON.stringify(v));
+            if (typeof clonedValue === "object") {
+              value = TrackableObject(clonedValue, { changes,propertyPath:fullPropertyPath });
+            } else {
+              value = clonedValue;
+            }
+          },
+        });
+      } else {
+        Object.defineProperty(_obj, propertyPath, {
+          get() {
+            return value;
+          },
+          set(v) {
+          
+            const oldValue = getObjPropertyByPath(_objShadow, propertyPath);
+            const fullPropertyPath = basePropertyPath? `${basePropertyPath}.${propertyPath}`:propertyPath;
+            if (oldValue === v) {
+              changes.delete(fullPropertyPath);
+            } else {
+              changes.set(fullPropertyPath, JSON.parse(JSON.stringify(v)));
+            }
+            const clonedValue = JSON.parse(JSON.stringify(v));
+            if (typeof clonedValue === "object") {
+              value = TrackableObject(clonedValue, { changes,propertyPath:fullPropertyPath });
+            } else {
+              value = clonedValue;
+            }
+          },
+        });
+      }
+    }
+  }
+
+  overrideSetterAndGetter(obj);
+
+  obj.t_changes = () => changes;
+  
   return obj;
 }
 
 const person = {
-  name: "test",
+  name: "person 1",
   child: {
     name: "child 1",
+    mom: {
+      name: "lil",
+    },
   },
 };
 
-const trackablePerson = new TrackableObjectOf(person);
-trackablePerson.name = "test 1";
+const trackablePerson = TrackableObject(person);
+
+trackablePerson.name = "person 2";
+trackablePerson.child.name = "child 2";
 trackablePerson.child.name = "child 1";
-console.log(trackablePerson.name, changes, trackablePerson.changes);
+trackablePerson.child.name = "child 3";
+trackablePerson.child.mom.name = {
+  prevName:'name 1',
+  nextName:'name 2'
+}
+
+trackablePerson.child.mom.name.prevName = 'name 3';
+// trackablePerson.name = "test 1";
+// trackablePerson.child = { name: "child 1 updated" };
+setTimeout(() => {
+  //   trackablePerson.child.name = "child 2 updated";
+  console.log(
+    trackablePerson.t_changes(),
+  );
+}, 1000);
